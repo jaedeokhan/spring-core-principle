e0df1f0ad39e29c95259f371c0d49fea8a692d8c
jaedeokhan <hjaedeok@gmail.com>
2023-03-11 19:04:23 +0900

    Add : 스프링 빈 설정 메타 정보 - BeanDefinition - Spring에서는 BeanDefinition으로 Bean 설정이 관리된다. - ApplicationContext의 구현체들 중 하나인   AnnotationConfigApplicationContext가 AnnotatedBeanDefinitionReader에게   빈 메타정보 생성 요청을 하고 AnnotatedBeanDefinitionReader는   AppConfig.class 설정 정보를 읽어와서 빈 정보를 생성하고 리턴한다.
    - Bean 생성 방법
     - 직접 생성(xml)
     - Bean 팩토리 메서드 생성(AppConfig)

---
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,46 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인 - 팩토리 메서드 주입")
+    void findFactoryApplicationBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인 - xml 직접 주입")
+    void findXmlApplicationBean() {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
+
