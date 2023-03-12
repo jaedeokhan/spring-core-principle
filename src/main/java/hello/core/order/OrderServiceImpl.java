99840b6ac2ae850190217037a78bb4a26c2c9a37
jaedeokhan <hjaedeok@gmail.com>
2023-03-12 22:37:02 +0900

    ADD : 조회 빈이 2개 이상 - 문제 - 스프링 수동으로 등록하는 해결 방법과 의존 관계 자동주입에서 해결하는   방법이 있다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,11 +7,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
