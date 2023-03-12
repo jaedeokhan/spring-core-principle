5386d9e048f41e5d3a5cd039535be45062075744
jaedeokhan <hjaedeok@gmail.com>
2023-03-12 16:29:47 +0900

    ADD : @Configuration과 싱글톤 - Spring에서는 @Bean으로 등록되어 있는 객체들을 여러 번 호출하는   구문이라도 싱글톤으로 한 번만 호출되게 자동으로 설정된다.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,18 +14,33 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository() && new RateDiscountPolicy()
+
+    // memberRepository가 3번 호출될 지 알았지만
+    // call AppConfig.memberService
+    //   - call AppConfig.memberRepository
+    // call AppConfig.orderService
+    //   - call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+
+    // 실제로는 한 번 호출이 된다.
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
