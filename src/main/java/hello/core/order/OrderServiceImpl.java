eea02ac0867c203f63016a2ef3ef4d68d646ee6d
jaedeokhan <hjaedeok@gmail.com>
2023-03-12 22:57:25 +0900

    ADD : @Autowired, @Quilifier, @Primary -> 두 개의 빈의 중복 회피 방법 - @Autowired    - 타입 매칭 시도 -> 필드 이름, 파라미터 이름 매칭 - @Quilifier    - 명시적으로 선언 필요 시 사용 -> 생성부, 사용부 둘 다 기재 - @Primary    - 생성부에서 간단하게 사용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,6 +4,8 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
